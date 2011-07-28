2269dc318605e8ac31b782f5f8a3364cec56a0e9
Sam Pullara <spullara@yahoo.com>
2011-07-28 14:54:59 -0700

    implement using method handles

--- src/main/java/redis/RedisServer.java
+++ src/main/java/redis/RedisServer.java
@@ -5,6 +5,9 @@ import com.sampullara.cli.Args;
 import com.sampullara.cli.Argument;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
@@ -31,9 +34,9 @@ public class RedisServer {
   private static Integer port = 6379;
 
   private static Map<String, Object> map = new ConcurrentHashMap<>();
-  private static Map<String, Code> commands = new HashMap<>();
+  private static Map<String, MethodHandle> commands = new HashMap<>();
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, IllegalAccessException {
     try {
       Args.parse(RedisServer.class, args);
     } catch (IllegalArgumentException e) {
@@ -83,37 +86,77 @@ public class RedisServer {
   private static Reply execute(Command command) {
     byte[][] arguments = command.getArguments();
     String verb = new String(arguments[0], Charsets.UTF_8).toLowerCase();
-    final Code code = commands.get(verb);
+    MethodHandle code = commands.get(verb);
     if (code == null) {
-      return new Reply.ErrorReply("Command not implemented: " + verb);
+      return new Reply.ErrorReply("Command not implemented or invalid arguments: " + verb);
     }
-    return code.call(arguments);
+    try {
+      switch(arguments.length) {
+        case 1:
+          return (Reply) code.invoke(Commands.O);
+        case 2:
+          return (Reply) code.invoke(Commands.O, arguments[1]);
+        case 3:
+          return (Reply) code.invoke(Commands.O, arguments[1], arguments[2]);
+        default:
+          return new Reply.ErrorReply("Invalid number of arguments: " + arguments.length);
+      }
+    } catch (Throwable throwable) {
+      logger.log(Level.SEVERE, "Failed", throwable);
+      return new Reply.ErrorReply("Failed: " + throwable);
+    }
   }
 
-  private static void init() {
-    commands.put("set", new Code() {
-      public Reply call(byte[][] arguments) {
-        if (arguments.length != 3) {
-          return new Reply.ErrorReply("Invalid number of arguments");
-        }
-        String key = new String(arguments[1], Charsets.UTF_8);
-        map.put(key, arguments[2]);
-        return new Reply.StatusReply("OK");
-      }
-    });
-    commands.put("get", new Code() {
-      public Reply call(byte[][] arguments) {
-        if (arguments.length != 2) {
-          return new Reply.ErrorReply("Invalid number of arguments");
-        }
-        String key = new String(arguments[1], Charsets.UTF_8);
-        Object value = map.get(key);
-        if (value instanceof byte[]) {
-          return new Reply.BulkReply((byte[]) value);
+  public static class Commands {
+    private Commands() {}
+    private final static Commands O = new Commands();
+    private final Reply.StatusReply OK = new Reply.StatusReply("OK");
+
+    public Reply append(byte[] k, byte[] s) {
+      synchronized (Commands.class) {
+        String key = $(k);
+        Object v = map.get(key);
+        if (v instanceof byte[]) {
+          byte[] b = (byte[]) v;
+          byte[] bytes = new byte[s.length + b.length];
+          System.arraycopy(b, 0, bytes, 0, b.length);
+          System.arraycopy(s, 0, bytes, b.length, s.length);
+          map.put(key, bytes);
+          return OK;
+        } else if (v == null) {
+          map.put(key, s);
+          return OK;
         } else {
-          return new Reply.ErrorReply("Invalid value type: " + value.getClass().getName());
+          return new Reply.ErrorReply("Invalid value type: " + v.getClass().getName());
         }
       }
-    });
+    }
+
+    public Reply set(byte[] k, byte[] v) {
+      String key = $(k);
+      map.put(key, v);
+      return OK;
+    }
+    public Reply get(byte[] k) {
+      String key = $(k);
+      Object value = map.get(key);
+      if (value instanceof byte[] || value == null) {
+        return new Reply.BulkReply((byte[]) value);
+      } else {
+        return new Reply.ErrorReply("Invalid value type: " + value.getClass().getName());
+      }
+    }
   }
+
+  private static void init() throws IllegalAccessException {
+    MethodHandles.Lookup lookup = MethodHandles.lookup();
+    for (Method method : Commands.class.getMethods()) {
+      commands.put(method.getName(), lookup.unreflect(method));
+    }
+  }
+
+  private static String $(byte[] k) {
+    return new String(k, Charsets.UTF_8);
+  }
+
 }
