b6ffd9cadde4f683eb09254f4af3474ec8546b48
Sam Pullara <spullara@yahoo.com>
2012-02-26 10:47:15 -0800

    test the pipeline client as well

--- client/src/test/java/client/RedisClientTest.java
+++ client/src/test/java/client/RedisClientTest.java
@@ -6,9 +6,11 @@ import redis.Command;
 import redis.client.RedisClient;
 import redis.client.SocketPool;
 import redis.reply.BulkReply;
+import redis.reply.StatusReply;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -43,6 +45,26 @@ public class RedisClientTest {
     RedisClient redisClient = new RedisClient(new SocketPool("localhost", 6379));
     for (int i = 0; i < CALLS; i++) {
       redisClient.set(Command.numToBytes(i, false), VALUE);
+    }
+    long end = System.currentTimeMillis();
+    System.out.println(CALLS * 1000 / (end - start) + " calls per second");
+  }
+
+  @Test
+  public void benchmarkPipeline() throws IOException, ExecutionException, InterruptedException {
+    int CALLS = 1000000;
+    long start = System.currentTimeMillis();
+    RedisClient redisClient = new RedisClient(new SocketPool("localhost", 6379));
+    RedisClient.Pipeline pipeline = redisClient.pipeline();
+    int PIPELINE_CALLS = 50;
+    Future<StatusReply>[] replies = new Future[PIPELINE_CALLS];
+    for (int i = 0; i < CALLS / PIPELINE_CALLS; i++) {
+      for (int j = 0; j < PIPELINE_CALLS; j++) {
+        replies[j] = pipeline.set(Command.numToBytes(i, false), VALUE);
+      }
+      for (int j = 0; j < PIPELINE_CALLS; j++) {
+        replies[j].get();
+      }
     }
     long end = System.currentTimeMillis();
     System.out.println(CALLS * 1000 / (end - start) + " calls per second");
