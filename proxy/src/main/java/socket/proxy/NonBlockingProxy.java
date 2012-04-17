0d0c97ce4eb721f1b99110381dbc245852b32f1d
Sam Pullara <spullara@twitter.com>
2012-04-16 21:02:44 -0700

    this is actually just a socket proxy

--- proxy/src/main/java/socket/proxy/NonBlockingProxy.java
+++ proxy/src/main/java/socket/proxy/NonBlockingProxy.java
@@ -1,4 +1,4 @@
-package redis.proxy;
+package socket.proxy;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -11,6 +11,8 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.sampullara.cli.Args;
 import com.sampullara.cli.Argument;
@@ -19,25 +21,38 @@ public class NonBlockingProxy {
   @Argument(alias = "p", description = "Port for this server to run")
   private static Integer port = 63790;
 
-  @Argument(alias = "r", description = "Redis server host:port")
-  private static String redisServer = "localhost:6379";
+  @Argument(alias = "r", description = "Server host:port")
+  private static String serverAddress = "localhost:6379";
 
   private static ExecutorService es = Executors.newCachedThreadPool();
-  public static final int REDIS_BUFFER_SIZE = 1024;
+  public static final int BUFFER_SIZE = 1024;
+
+  private static final Logger logger = Logger.getLogger("Proxy");
+
+  private static abstract class Handler<A> implements CompletionHandler<Integer, A> {
+    @Override
+    public void failed(Throwable exc, A attachment) {
+      error(exc, attachment);
+    }
+  }
+
+  private static void error(Throwable exc, Object attachment) {
+    logger.log(Level.WARNING, "IO failure in " + attachment, exc);
+  }
 
   public static void main(String[] args) throws IOException, InterruptedException {
-    final String redisHost;
-    final int redisPort;
+    final String host;
+    final int port;
     try {
-      Args.parse(BlockingProxy.class, args);
-      String[] split = redisServer.split(":");
+      Args.parse(NonBlockingProxy.class, args);
+      String[] split = serverAddress.split(":");
       if (split.length != 2) {
         throw new IllegalArgumentException("host:port");
       }
-      redisHost = split[0];
-      redisPort = Integer.parseInt(split[1]);
+      host = split[0];
+      port = Integer.parseInt(split[1]);
     } catch (IllegalArgumentException e) {
-      Args.usage(BlockingProxy.class);
+      Args.usage(NonBlockingProxy.class);
       System.exit(1);
       return;
     }
@@ -45,7 +60,7 @@ public class NonBlockingProxy {
     CountDownLatch done = new CountDownLatch(1);
 
     final AsynchronousServerSocketChannel listener =
-            AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(port));
+            AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(NonBlockingProxy.port));
     final Queue<ByteBuffer> queue = new ConcurrentLinkedQueue<>();
 
     listener.accept(null, new CompletionHandler<AsynchronousSocketChannel,Void>() {
@@ -53,70 +68,51 @@ public class NonBlockingProxy {
         // accept the next connection
         listener.accept(null, this);
 
-        final AsynchronousSocketChannel redis;
+        final AsynchronousSocketChannel server;
         try {
-          redis = AsynchronousSocketChannel.open();
-          redis.connect(new InetSocketAddress(redisHost, redisPort)).get();
+          server = AsynchronousSocketChannel.open();
+          server.connect(new InetSocketAddress(host, port)).get();
         } catch (Exception e) {
-          e.printStackTrace();
-          try {
-            listener.close();
-          } catch (IOException e1) {
-            e1.printStackTrace();
-          }
+          error(e, "connect failed: " + serverAddress);
+          System.exit(1);
           return;
         }
 
-        read(client, redis);
-        read(redis, client);
+        read(client, server);
+        read(server, client);
+      }
+
+      @Override
+      public void failed(Throwable exc, Void attachment) {
+        error(exc, "accept");
+        System.exit(1);
       }
 
       private ByteBuffer getBuffer() {
         ByteBuffer poll = queue.poll();
         if (poll == null) {
-          return ByteBuffer.allocate(REDIS_BUFFER_SIZE);
+          return ByteBuffer.allocate(BUFFER_SIZE);
         }
         return poll;
       }
 
       private void read(final AsynchronousSocketChannel reader, AsynchronousSocketChannel writer) {
         final ByteBuffer buffer = getBuffer();
-        reader.read(buffer, writer, new CompletionHandler<Integer, AsynchronousSocketChannel>() {
-
+        reader.read(buffer, writer, new Handler<AsynchronousSocketChannel>() {
           @Override
           public void completed(Integer result, AsynchronousSocketChannel writer) {
             if (result == -1) {
               return;
             }
-            writer.write((ByteBuffer) buffer.flip(), buffer, new CompletionHandler<Integer, ByteBuffer>() {
+            writer.write((ByteBuffer) buffer.flip(), buffer, new Handler<ByteBuffer>() {
               @Override
               public void completed(Integer result, ByteBuffer attachment) {
                 queue.add((ByteBuffer) buffer.clear());
               }
-
-              @Override
-              public void failed(Throwable exc, ByteBuffer attachment) {
-                exc.printStackTrace();
-              }
             });
             read(reader, writer);
           }
-
-          @Override
-          public void failed(Throwable exc, AsynchronousSocketChannel attachment) {
-            exc.printStackTrace();
-            try {
-              attachment.close();
-              reader.close();
-            } catch (IOException e) {
-              e.printStackTrace();
-            }
-          }
         });
-      }
-
-      public void failed(Throwable exc, Void att) {
-        exc.printStackTrace();
       }
     });
 
