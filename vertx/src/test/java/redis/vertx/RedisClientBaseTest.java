ba26443aed78153a807b2b1990c4945f8cc1a3db
Sam Pullara <spullara@twitter.com>
2012-10-04 20:39:54 -0700

    add a benchmark for synchronous sets

--- vertx/src/test/java/redis/vertx/RedisClientBaseTest.java
+++ vertx/src/test/java/redis/vertx/RedisClientBaseTest.java
@@ -1,7 +1,10 @@
 package redis.vertx;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+
+import com.google.common.base.Charsets;
 
 import org.junit.Test;
 import org.vertx.java.core.Handler;
@@ -14,6 +17,7 @@ import redis.netty.StatusReply;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+import static redis.util.Encoding.numToBytes;
 
 public class RedisClientBaseTest {
   @Test
@@ -92,5 +96,34 @@ public class RedisClientBaseTest {
     assertNotNull(getResult.get());
   }
 
+  private static final byte[] VALUE = "value".getBytes(Charsets.UTF_8);
+  private static final long CALLS = 100000l;
+
+  @Test
+  public void testBenchmark() throws InterruptedException {
+    long start = System.currentTimeMillis();
+    final byte[] SET = "SET".getBytes();
+    final AtomicInteger num = new AtomicInteger(0);
+    final CountDownLatch countDownLatch = new CountDownLatch(1);
+    RedisClientBase.connect("localhost", 6379, Vertx.newVertx(), new Handler<RedisClientBase>() {
+      @Override
+      public void handle(final RedisClientBase redisClient) {
+        Handler<Reply> replyHandler = new Handler<Reply>() {
+          @Override
+          public void handle(Reply statusReply) {
+            int current = num.getAndIncrement();
+            if (current == CALLS) {
+              countDownLatch.countDown();
+            } else {
+              redisClient.send(new Command(SET, numToBytes(current), VALUE), this);            }
+          }
+        };
+        replyHandler.handle(new StatusReply("GO"));
+      }
+    });
+    countDownLatch.await();
+    long end = System.currentTimeMillis();
+    System.out.println("Vertx: " + (CALLS * 1000) / (end - start) + " calls per second");
+  }
 }
 
